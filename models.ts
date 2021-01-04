3d33297a46fff93ea003287c82860bb7509fd33e
yenobi <koshman.animys@gmail.com>
2021-01-04 22:24:00 +0200

    add exports for X and O

--- models.ts
+++ models.ts
@@ -3,8 +3,8 @@
 
 // todo: after finish -> check if i could implement the game with linear
 // data structures (string with calculated shift)
-type X = 'x';
-type O = 'o';
+export type X = 'x';
+export type O = 'o';
 export type EmptyCell = '_';
 export type Cell = EmptyCell | X | O;
 export type Row = [Cell, Cell, Cell];
