6ad19988dde96dcacefb5bfb165e57f81d37139d
yenobi <koshman.animys@gmail.com>
2021-01-08 18:58:06 +0200

    add run game draft files

---
+++ run-game.spec.ts
@@ -0,0 +1,5 @@
+describe('run game', () => {
+  it('should play game from the very start', () => {});
+
+  it('should play game from the input field', () => {});
+});
---
+++ run-game.ts
@@ -0,0 +1,23 @@
+export function runGame(): GameField {}
+// function runGame(field: GameField): GameField {
+//   const currentField = createGameField();
+// // fill in to prevent win of the opposite
+// // random move
+//   return updatedField;
+// }
+
+// runGame()
+// 1. with empty field
+// - create field
+// - toss a coin for the first player
+// - make move
+// - side effect logging every move -> additional flag
+// - log the result
+
+
+// 2. with given field
+// - create field from the input
+// - getTheCurrentPlayer
+// - make a move
+// - side effect logging -> additional flag
+// - log the result
